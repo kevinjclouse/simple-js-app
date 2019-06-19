900d80521200725dd44b27383afa945856e53af4
kevinjclouse <kevinjclouse@users.noreply.github.com>
2019-06-19 17:16:01 -0500

    Made a list in JS
    Made my Pokemon list for my app 7 started the Journey of learning JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
-var repository = [1, 2, 3];
-
-var Pokemon = {
-  name: 'Bulbasaur',
-  height: 7,
-  type: ['grass', 'poison'],
-  child: {
-    name: 'Squirtle',
-    height: 5,
-    type: ['grass', 'water'],
-    child: {
-      name: 'Pidgey',
-      height: 3,
-      type: ['grass', 'rock']
-    }
+var repository = [
+  {
+    name: 'Bulbasaur',
+    height: 7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Pidgey',
+    height: 3,
+    types: ['rock', 'ice']
+  },
+  {
+    name: 'Metapod',
+    height: .7,
+    types: ['fire']
   }
-};
+];
 
-for (var i = 0; i <= repository.length; i++) {
-  document.write('Bulbasaur(height:7)')
-  document.write('Squirtle(height:5)')
-  document.write('Pidgey(height:3)')
+for (var i = 0; i < repository.length; i++) {
+  var bigPoke = ""
+  if (repository[i].height > 6){
+    bigPoke = "Wow, that's big!"
+  }
+  document.write('<p>' + '<strong>' + repository[i].name + '</strong>' + ' -' + ' Height: ' + repository[i].height + ' - Abilities: ' + repository[i].types + ' ' + bigPoke);
 }
