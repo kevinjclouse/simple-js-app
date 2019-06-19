8fd29cc522d90d4adacc8d47dff9558fac6226a0
kevinjclouse <kevinjclouse@users.noreply.github.com>
2019-06-19 07:56:37 -0500

    Update the Script
    Added an Array with objects for a list

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-alert('Hello World My Name is Kevin');
+var repository = [1, 2, 3];
+
+var Pokemon = {
+  name: 'Bulbasaur',
+  height: 7,
+  type: ['grass', 'poison'],
+  child: {
+    name: 'Squirtle',
+    height: 5,
+    type: ['grass', 'water'],
+    child: {
+      name: 'Pidgey',
+      height: 3,
+      type: ['grass', 'rock']
+    }
+  }
+};
+
+for (var i = 0; i <= repository.length; i++) {
+  document.write('Bulbasaur(height:7)')
+  document.write('Squirtle(height:5)')
+  document.write('Pidgey(height:3)')
+}
