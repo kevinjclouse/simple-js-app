a4390b231bc94144345bf6d9188ff55bdc933d6a
kevinjclouse <kevinjclouse@users.noreply.github.com>
2019-06-25 21:56:24 -0500

    Added an IIFE to JS
    Added IIFE to a new Variable called pokemonRepository with the ( Original ) Variable called repository inside the IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,49 @@
-var repository = [
-  {
-    name: 'Bulbasaur',
-    height: 7,
-    types: ['grass', 'poison']
-  },
-  {
-    name: 'Pidgey',
-    height: 3,
-    types: ['rock', 'ice']
-  },
-  {
-    name: 'Metapod',
-    height: .7,
-    types: ['fire']
+var pokemonRepository = (function() {
+  var repository = [{
+      name: 'Bulbasaur',
+      height: 7,
+      types: ['grass', 'poison']
+    },
+    {
+      name: 'Pidgey',
+      height: 3,
+      types: ['rock', 'ice']
+    },
+    {
+      name: 'Metapod',
+      height: .7,
+      types: ['fire']
+    }
+  ];
+//
+  function add(pokemon) {
+    repository.push(pokemon);
   }
-];
+//
+  function getAll() {
+    return repository;
+  }
+//
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+//
+pokemonRepository.add({ name: 'Eevee' });
+repository.forEach(function(currentName) {
+  document.write(pokemonRepository.getAll()'<p>' + '<strong>' + '<h1>' + currentName.name + ' : ' + '</h1>' + '</strong>' + ' Height ' + '<strong>' + ' : ' + '</strong>' + currentName.height + ' Type ' + ' = ' + currentName.types)
+});
+
 
 // for (var i = 0; i < repository.length; i++) {
 //   var bigPoke = ""
 //   if (repository[i].height > 6) {
 //     bigPoke = "Wow, that's big!"
-  // }
-  // document.write('<p>' + '<strong>' + repository[i].name + '</strong>' + ' -' + ' Height: ' + repository[i].height + ' - Abilities: ' + repository[i].types + ' ' + bigPoke);
+// }
+// document.write('<p>' + '<strong>' + repository[i].name + '</strong>' + ' -' + ' Height: ' + repository[i].height + ' - Abilities: ' + repository[i].types + ' ' + bigPoke);
 // }
 
-repository.forEach(function(currentName){
-  document.write('<p>' + '<strong>' + '<h1>' + currentName.name + ' : ' + '</h1>' + '</strong>' + ' Height ' + '<strong>' + ' : ' + '</strong>' + currentName.height + ' Type ' + ' = ' + currentName.types);
-});
+// repository.forEach(function(currentName) {
+//   document.write('<p>' + '<strong>' + '<h1>' + currentName.name + ' : ' + '</h1>' + '</strong>' + ' Height ' + '<strong>' + ' : ' + '</strong>' + currentName.height + ' Type ' + ' = ' + currentName.types)
+// });
